024060edda17d3dd85e3e80ee6a63315ad313b6e
LasithaAmarasinghe <amarasinghelra@gmail.com>
2025-08-22 12:27:45 +0530

    make transformers and inspections appear in 2 tabs

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -8,16 +8,25 @@ import InspectionList from './InspectionList';
 import './App.css';
 
 export default function App() {
+  const [page, setPage] = React.useState('transformers');
   return (
     <div className="flex min-h-screen">
       <Sidebar />
       <div className="flex-1 max-h-screen overflow-auto">
         <UpperBar />
         <div className="p-8">
-          <AddTransformerModal />
-          <AddInspection />
-          <TransformerList />
-          <InspectionList />
+          {page === 'transformers' && (
+            <>
+              <AddTransformerModal />
+              <TransformerList page={page} setPage={setPage} />
+            </>
+          )}
+          {page === 'inspections' && (
+            <>
+              <AddInspection />
+              <InspectionList page={page} setPage={setPage} />
+            </>
+          )}
         </div>
       </div>
     </div>
