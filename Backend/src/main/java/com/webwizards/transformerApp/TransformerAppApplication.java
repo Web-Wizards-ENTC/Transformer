d047f03bcd052b23002604fbebaa8c312a294186
RuchiraAbeywardhane <ruchiraabeywardhane08@gmail.com>
2025-08-23 10:27:49 +0530

    Spring Initializer

---
+++ Backend/src/main/java/com/webwizards/transformerApp/TransformerAppApplication.java
@@ -0,0 +1,13 @@
+package com.webwizards.transformerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransformerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransformerAppApplication.class, args);
+	}
+
+}
